fec025c832a00afb86f998f17d71b39a5d25575a
federicobac <federico.baccaro1@gmail.com>
2026-08-19 14:09:15 +0200

    Add comments to post page
    Fetch comments for the selected post and render them beneath the post content. This updates the post detail view to show a comment section with each comment's user and message.

--- src/PostPage.tsx
+++ src/PostPage.tsx
@@ -1,16 +1,24 @@
 import {useParams} from "react-router";
 import {useEffect, useState} from "react";
 import type {Post} from "./models/Post.ts";
+import type {Comment} from "./models/Comment.ts";
 
 export function PostPage() {
 
     const {id} = useParams<{ id: string }>();
+
     const [post, setPost] = useState<Post[]>([]);
+    const [comments, setComments] = useState<Comment[]>([]);
 
     useEffect(() => {
         fetch(`https://dummyjson.com/posts/${id}`)
             .then(response => response.json())
             .then(json => setPost([json]))
+
+        fetch(`https://dummyjson.com/posts/${id}/comments`)
+            .then(response => response.json())
+            .then(json => setComments(json.comments));
+
     }, [id]);
 
 
@@ -20,6 +28,15 @@ export function PostPage() {
                 <div key={post.id}>
                     <h1>{post.title}</h1>
                     <p>{post.body}</p>
+                </div>
+            )}
+
+            <h2>Comment Section</h2>
+
+            {comments.map(comment =>
+                <div key={comment.id}>
+                    <h3>{comment.user.username}</h3>
+                    <p>{comment.body}</p>
                 </div>
             )}
         </>
